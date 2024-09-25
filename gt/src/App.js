bbb357e541a99e85cd794c757331fbfd0925968b
Saurabh S <surabhsuresh20@gmail.com>
2024-09-25 19:11:16 +0530

    heading

--- gt/src/App.js
+++ gt/src/App.js
@@ -1,11 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import TitleCard from './TitleCard';
+import './output.css';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <link href="./output.css" rel="stylesheet" />
       <TitleCard />
 
     </div>
