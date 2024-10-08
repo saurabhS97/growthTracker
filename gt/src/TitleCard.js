4644c408913b36215dd2a1393a5a5abda9957873
Saurabh S <surabhsuresh20@gmail.com>
2024-10-08 20:49:49 +0530

    otp screen

--- gt/src/TitleCard.js
+++ gt/src/TitleCard.js
@@ -9,7 +9,7 @@ function TitleCard() {
                         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                             <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                                 <span class="absolute -inset-0.5"></span>
-                                <span class="sr-only">Open1 main menu</span>
+                                <span class="sr-only">Open1 main menu </span>
 
                                 <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
